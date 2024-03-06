4846570eb460538c3269666f850fe06fb72f1483
Rafael Fraga <rafalfraga@gmail.com>
2024-03-06 15:16:21 -0300

    Adição de novos formulários + campo de médico

---
+++ PublicoRestrito/Cadastros/script.js
@@ -0,0 +1,8 @@
+document.getElementById('cargo').onchange = function () {
+    var medicoCampos = document.getElementById('medicoCampos');
+    if (this.value === 'medico') {
+        medicoCampos.style.display = 'block';
+    } else {
+        medicoCampos.style.display = 'none';
+    }
+};
