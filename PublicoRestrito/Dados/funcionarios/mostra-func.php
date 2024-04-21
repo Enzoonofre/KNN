e3c226d5b4bbdf5c508c0ecf1d858f0e3ae7a34a
DaviCampos09 <davimcampos09@ufu.br>
2024-04-21 20:21:44 -0300

    tudo pronto menos mostra paciente

--- PublicoRestrito/Dados/funcionarios/mostra-func.php
+++ PublicoRestrito/Dados/funcionarios/mostra-func.php
@@ -1,10 +1,10 @@
 <?php
- require 'funcionario.php';
- require 'conexaoMysql.php';
+require 'funcionario.php';
+require 'conexaoMysql.php';
 
 $pdo = mysqlConnect();
 
- $arrayFuncionarios = Funcionario::GetData($pdo);
+$arrayFuncionarios = Funcionario::GetData($pdo);
 ?>
 
 <!doctype html>
@@ -14,19 +14,34 @@ $pdo = mysqlConnect();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Funcionários Cadastrados</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <style>
-    body {
-      padding-top: 2rem;
-    }
-
-    img {
-      width: 20px;
-    }
-  </style>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
+  <header>
+    <div class="item_header">
+      <img src="../imagens/logo2.jpg" alt="Logo Clínica" id="logo">
+    </div>
+  </header>
+
+  <nav>
+    <div class="conteiner">
+      <div class="item">
+        <a href="../../homeRestrito.php">Home</a>
+      </div>
+      <div class="item">
+        <a href="../../Cadastros/cadastroFunc.html">Cadastro de Funcionarios</a>
+      </div>
+      <div class="item">
+        <a href="../../Cadastros/cadastroPaciente.html">Cadastro de Pacientes</a>
+      </div>
+      <div class="item">
+        <a href="../dados.php">Listagem de Dados</a>
+      </div>
+    </div>
+  </nav>
 
   <div class="container">
     <h3>Funcionários Cadastrados</h3>
